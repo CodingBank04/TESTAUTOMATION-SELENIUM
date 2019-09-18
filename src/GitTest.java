888eca507dd4a85fa3aa30f8e43657408cec106a
GURU-JAVA <54605558+beehive04@users.noreply.github.com>
2019-09-17 19:13:54 -0600

    My Latest Commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,7 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("First Step");
         System.out.println("first step");
-        System.out.println("Second STP");
-        System.out.println("third STP");
+
     }
 }
