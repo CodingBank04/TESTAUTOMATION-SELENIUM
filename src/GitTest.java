646cf1b7c1afb153bccaea58e899999353ff58fe
GURU-JAVA <54605558+beehive04@users.noreply.github.com>
2019-09-17 18:51:04 -0600

    My SECOND Commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,8 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("First Step");
-        System.out.println("Second step");
+        System.out.println("first step");
+        System.out.println("Second STP");
+        System.out.println("third STP");
     }
 }
