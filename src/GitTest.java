1ae236bd2bc357b5d6ae62d30b96fad3fb335210
GURU-JAVA <54605558+beehive04@users.noreply.github.com>
2019-09-17 19:29:01 -0600

    Update GitTest.java

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("First Step");
         System.out.println("first step");
+        System.out.pringln("6 step");
 
     }
 }
