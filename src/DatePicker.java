7bd815097c92f50bb49c76191680c86d9563d508
GURU-JAVA <54605558+beehive04@users.noreply.github.com>
2019-09-28 18:10:06 -0600

    15.th commit

--- src/DatePicker.java
+++ src/DatePicker.java
@@ -4,13 +4,17 @@ import org.openqa.selenium.WebDriver;
 public class DatePicker {
     public static void main(String[] args) throws InterruptedException {
 
-        Browser myBrowser = new Browser();
-        WebDriver driver = myBrowser.invokeChromeBrowse();
-        driver.get("https://www.expedia.com/");
-        Thread.sleep(1000);
-        driver.findElement(By.id("package-departing-hp-package")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.cssSelector("button[data-year='2019'][data-month='9'][data-day='30']")).click();
+            Browser myBrovser = new Browser();
+            WebDriver driver = myBrovser.invokeChromeBrowse();
+            driver.get("http://www.expedia.com");
+            Thread.sleep(2000);
+            driver.findElement(By.id("package-departing-hp-package")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.cssSelector("button.datepicker-paging.datepicker-next.btn-paging.btn-secondary.next")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.cssSelector("button.datepicker-paging.datepicker-next.btn-paging.btn-secondary.next")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.cssSelector("button[data-year='2019'][data-month='11'][data-day='29']")).click();
 
+        }
     }
-}
